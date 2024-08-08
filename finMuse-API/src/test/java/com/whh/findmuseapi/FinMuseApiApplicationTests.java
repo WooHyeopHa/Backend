51e9cf17e799579c00e71f33125ca55153d2ec32
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-08 12:20:55 +0900

    [feat] API 서버 프로젝트 생성

---
+++ finMuse-API/src/test/java/com/whh/findmuseapi/FinMuseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whh.findmuseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinMuseApiApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
