510835dbbca2642fffd84862c6559374302ec1d9
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-08 13:50:28 +0900

    [feat] chatting 프로젝트 생성

---
+++ findMuse-chatting/src/main/java/com/whh/findmusechatting/FindMuseChattingApplication.java
@@ -0,0 +1,13 @@
+package com.whh.findmusechatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMuseChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMuseChattingApplication.class, args);
+	}
+
+}
