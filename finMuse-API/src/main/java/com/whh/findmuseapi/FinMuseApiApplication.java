28efae3d1040630232c4b1520e4c4171ed659dba
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-08 12:52:42 +0900

    [Feat] Spring Boot 메인 서버 프로젝트 생성 (#12)
    * [feat] API 서버 프로젝트 생성
    
    * [fix] CICD 테스트를 위한 gradle 수정
    
    * [feat] Dockerfile 생성
    
    * [feat] Jenkinsfile 생성
    
    * [remove] 실수로 push한 demo 프로젝트 삭제

---
+++ finMuse-API/src/main/java/com/whh/findmuseapi/FinMuseApiApplication.java
@@ -0,0 +1,13 @@
+package com.whh.findmuseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinMuseApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(FinMuseApiApplication.class, args);
+    }
+    
+}
