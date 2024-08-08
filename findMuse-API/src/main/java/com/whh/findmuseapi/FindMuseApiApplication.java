b0d4e89bef81eda2e74b801f765be5a3356f61ec
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-08 14:50:30 +0900

    [fix] 폴더 구조 변경

---
+++ findMuse-API/src/main/java/com/whh/findmuseapi/FindMuseApiApplication.java
@@ -0,0 +1,13 @@
+package com.whh.findmuseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMuseApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(FindMuseApiApplication.class, args);
+    }
+    
+}
