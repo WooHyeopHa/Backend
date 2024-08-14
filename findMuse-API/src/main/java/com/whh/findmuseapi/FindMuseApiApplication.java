3a3a59899b2106a768444882d7a5ed402850afee
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-14 17:23:24 +0900

    [feat] feign 동작되도록 설정

--- findMuse-API/src/main/java/com/whh/findmuseapi/FindMuseApiApplication.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/FindMuseApiApplication.java
@@ -2,7 +2,9 @@ package com.whh.findmuseapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class FindMuseApiApplication {
     
