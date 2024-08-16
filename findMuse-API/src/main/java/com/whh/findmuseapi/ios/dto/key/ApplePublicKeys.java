84245a32bffc3ecb6301024d245a767c156d254d
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-16 17:57:24 +0900

    [feat] publicKey list dto

---
+++ findMuse-API/src/main/java/com/whh/findmuseapi/ios/dto/key/ApplePublicKeys.java
@@ -0,0 +1,11 @@
+package com.whh.findmuseapi.ios.dto.key;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApplePublicKeys {
+    private List<ApplePublicKey> keys;
+}
