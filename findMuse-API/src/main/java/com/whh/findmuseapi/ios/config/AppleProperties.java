c796648a7d4d137c2f21c03940f27088d6638f6c
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-16 17:56:51 +0900

    [feat] property 설정

---
+++ findMuse-API/src/main/java/com/whh/findmuseapi/ios/config/AppleProperties.java
@@ -0,0 +1,18 @@
+package com.whh.findmuseapi.ios.config;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "apple")
+public class AppleProperties {
+    private String teamId;
+    private String loginKey;
+    private String clientId;
+    private String redirectUrl;
+    private String keyPath;
+}
