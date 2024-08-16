cf4af317e518519617488ada2facbeaeb87f2914
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-16 18:05:02 +0900

    [fix] 임시 파일 생성

--- findMuse-API/src/main/java/com/whh/findmuseapi/user/service/UserService.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/user/service/UserService.java
@@ -1,35 +1,13 @@
 package com.whh.findmuseapi.user.service;
 
-import com.whh.findmuseapi.ios.dto.OAuth2Request;
-import com.whh.findmuseapi.user.entity.User;
-import com.whh.findmuseapi.user.repository.UserrRepository;
+import com.whh.findmuseapi.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     
-    private final UserrRepository userrRepository;
-    
-    @Transactional(readOnly = true)
-    public User findByAccountId(String accountId) {
-        return userrRepository.findByAccountId(accountId).orElseThrow();
-    }
-    
-    @Transactional
-    public User saveIfNewUser(OAuth2Request oAuth2Request) {
-        User existingUser = findByAccountId(oAuth2Request.getAccountId());
-        if (existingUser != null) {
-            return existingUser;
-        }
-        
-        User newUser = new User();
-        newUser.setAccountId(oAuth2Request.getAccountId());
-        newUser.setName(oAuth2Request.getName());
-        newUser.setEmail(oAuth2Request.getEmail());
-        
-        return userrRepository.save(newUser);
-    }
+    private final UserRepository userRepository;
+
 }
