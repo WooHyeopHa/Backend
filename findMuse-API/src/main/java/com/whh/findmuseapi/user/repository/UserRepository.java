d29536c04a15fa25c558e45d0217bd9acb1c99e6
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-16 18:04:29 +0900

    [feat] email로 찾기 추가

--- findMuse-API/src/main/java/com/whh/findmuseapi/user/repository/UserRepository.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/user/repository/UserRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserrRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     
     Optional<User> findByAccountId(String accountId);
+    Optional<User> findByEmail(Optional<String> email);
+    Optional<User> findByEmail(String email);
 }
