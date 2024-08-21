9ab2c913ba0c34df057c8ae06f47ac16139c76db
Hyeok-Gyu Lee <e_hyeok9@kookmin.ac.kr>
2024-08-21 20:58:39 +0900

    [fix] setter annotaion 제거

--- findMuse-API/src/main/java/com/whh/findmuseapi/ios/dto/key/ApplePublicKey.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/ios/dto/key/ApplePublicKey.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class ApplePublicKey {
     private String kty;
     private String kid;
--- findMuse-API/src/main/java/com/whh/findmuseapi/ios/dto/key/ApplePublicKeys.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/ios/dto/key/ApplePublicKeys.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class ApplePublicKeys {
     private List<ApplePublicKey> keys;
 }
