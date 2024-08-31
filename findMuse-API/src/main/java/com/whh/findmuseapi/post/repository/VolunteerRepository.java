f4e77319823ae0c8f0e02caf1def2b8588ccbba7
minseo <sa4777@naver.com>
2024-08-31 19:12:13 +0900

    [feat] 모집글 vlounteer 리스트 조회

--- findMuse-API/src/main/java/com/whh/findmuseapi/post/repository/VolunteerRepository.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/post/repository/VolunteerRepository.java
@@ -4,10 +4,13 @@ import com.whh.findmuseapi.common.constant.Infos;
 import com.whh.findmuseapi.post.entity.Post;
 import com.whh.findmuseapi.post.entity.Volunteer;
 import com.whh.findmuseapi.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long> {
     Long countByPostIdAndStatusAndActiveStatus(Long postId, Infos.InvieteStatus status, boolean activeStatus);
 
     boolean existsByUserAndPost(User user, Post post);
-}
+
+    List<Volunteer> findByPostIdAndActiveStatusTrueAndStatus(Long postId, Infos.InvieteStatus status);
+    List<Volunteer> findByPostIdAndActiveStatusTrueAndStatusNot(Long postId, Infos.InvieteStatus status);}
