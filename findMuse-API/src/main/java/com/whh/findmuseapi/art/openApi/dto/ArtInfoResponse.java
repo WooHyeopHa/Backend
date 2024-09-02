6009f513a895dcfd9ff65a845972efc8f0503ff1
kyu-hyun <dlrbgus722@naver.com>
2024-09-02 23:50:22 +0900

    feat : DTO 수정 및 생성

--- findMuse-API/src/main/java/com/whh/findmuseapi/art/openApi/dto/ArtInfoResponse.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/art/openApi/dto/ArtInfoResponse.java
@@ -1,23 +1,43 @@
 package com.whh.findmuseapi.art.openApi.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
-@Getter
+
+@AllArgsConstructor
 @NoArgsConstructor
+@XmlRootElement(name = "dbs")
 public class ArtInfoResponse {
 
-    @JsonProperty("db")
-    private List<Id> idList;
+    private List<Db> dbList;
+
+    public void setDbList(List<Db> dbList) {
+        this.dbList = dbList;
+    }
+
+    @XmlElement(name = "db")
+    public List<Db> getDbList() {
+        return dbList;
+    }
 
     @NoArgsConstructor
-    @Getter
-    public static class Id {
+    @XmlRootElement(name = "db")
+    public static class Db {
 
-        @JsonProperty("mt20id")
-        private String artId;
+//        @JsonProperty("mt20id")
+        private String id;
+
+        @XmlElement(name = "mt20id")
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
     }
 }
