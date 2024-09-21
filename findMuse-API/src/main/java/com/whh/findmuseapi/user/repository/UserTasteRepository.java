658e2b8033b1705753660076afb901e08e51161a
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2024-09-21 22:02:03 +0900

    [feat] repository 생성

---
+++ findMuse-API/src/main/java/com/whh/findmuseapi/user/repository/UserTasteRepository.java
@@ -0,0 +1,9 @@
+package com.whh.findmuseapi.user.repository;
+
+import com.whh.findmuseapi.user.entity.User;
+import com.whh.findmuseapi.user.entity.UserTaste;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserTasteRepository extends JpaRepository<UserTaste, Long> {
+    void deleteByUser(User user);
+}
