b5225f2b959cd282781d75b8217e4be12c723b33
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2024-09-22 17:16:29 +0900

    [fix] 순환 참조 에러 -> 주석 처리

--- findMuse-API/src/main/java/com/whh/findmuseapi/post/controller/VolunteerController.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/post/controller/VolunteerController.java
@@ -1,98 +1,98 @@
-package com.whh.findmuseapi.post.controller;
-
-import com.whh.findmuseapi.common.constant.ResponseCode;
-import com.whh.findmuseapi.common.util.ApiResponse;
-import com.whh.findmuseapi.post.dto.response.VolunteerPostListResponse;
-import com.whh.findmuseapi.post.service.VolunteerService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * class: VolunteerController.
- * 모집글 지워자에 대한 컨트롤러입니다.
- *
- * @author devminseo
- * @version 8/31/24
- */
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/volunteer")
-public class VolunteerController {
-    private final VolunteerService volunteerService;
-
-    /**
-     * 모집글에 지원을 신청하는 엔드포인트
-     *
-     * @param postId 모집글 ID
-     * @param userId 지원자 ID
-     * @return 지원 결과
-     */
-    @PostMapping("/apply")
-    public ApiResponse<?> applyVolunteer(@RequestParam Long postId, @RequestParam Long userId) {
-        volunteerService.applyVolunteer(postId, userId);
-        return ApiResponse.createSuccessWithNoContent(ResponseCode.SUCCESS);
-    }
-
-    /**
-     * 모집글에 지원 취소하는 엔드포인트
-     *
-     * @param volunteerId 지원자 ID
-     * @param userId 모집글 작성자 ID
-     * @return 취소 결과
-     */
-    @DeleteMapping("/cancel")
-    public ApiResponse<?> cancelVolunteer(@RequestParam Long volunteerId, @RequestParam Long userId) {
-        volunteerService.cancelVolunteer(volunteerId, userId);
-        return ApiResponse.createSuccessWithNoContent(ResponseCode.SUCCESS);
-    }
-
-    /**
-     * 모집글 지원자 목록 조회 엔드포인트 (참여자와 대기자)
-     *
-     * @param postId 모집글 ID
-     * @param userId 작성자 ID
-     * @return 지원자 목록
-     */
-    @GetMapping("/post/{postId}/volunteers")
-    public ApiResponse<?> getPostVolunteerList(@PathVariable Long postId, @RequestParam Long userId) {
-        VolunteerPostListResponse response = volunteerService.getPostVolunteerList(postId, userId);
-        return ApiResponse.createSuccess(ResponseCode.SUCCESS, response);
-    }
-
-
-    /**
-     * 모집글의 지원자를 수락하는 엔드포인트
-     *
-     * @param postId 모집글 ID
-     * @param writerId 작성자 ID
-     * @param targetId 지원자 ID
-     * @return 수락 결과
-     */
-    @PostMapping("/{postId}/accept")
-    public ApiResponse<?> acceptVolunteer(@PathVariable Long postId, @RequestParam Long writerId, @RequestParam Long targetId) {
-        volunteerService.acceptVolunteer(postId, writerId, targetId);
-        return ApiResponse.createSuccessWithNoContent(ResponseCode.SUCCESS);
-    }
-
-    /**
-     * 모집글의 지원자를 거절하는 엔드포인트
-     *
-     * @param postId 모집글 ID
-     * @param writerId 작성자 ID
-     * @param targetId 지원자 ID
-     * @return 거절 결과
-     */
-    @PostMapping("/{postId}/refuse")
-    public ApiResponse<?> refuseVolunteer(@PathVariable Long postId, @RequestParam Long writerId, @RequestParam Long targetId) {
-        volunteerService.refusalVolunteer(postId, writerId, targetId);
-        return ApiResponse.createSuccessWithNoContent(ResponseCode.SUCCESS);
-    }
-
-}
+//package com.whh.findmuseapi.post.controller;
+//
+//import com.whh.findmuseapi.common.constant.ResponseCode;
+//import com.whh.findmuseapi.common.util.ApiResponse;
+//import com.whh.findmuseapi.post.dto.response.VolunteerPostListResponse;
+//import com.whh.findmuseapi.post.service.VolunteerService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * class: VolunteerController.
+// * 모집글 지워자에 대한 컨트롤러입니다.
+// *
+// * @author devminseo
+// * @version 8/31/24
+// */
+//
+//@RestController
+//@RequiredArgsConstructor
+//@RequestMapping("/volunteer")
+//public class VolunteerController {
+//    private final VolunteerService volunteerService;
+//
+//    /**
+//     * 모집글에 지원을 신청하는 엔드포인트
+//     *
+//     * @param postId 모집글 ID
+//     * @param userId 지원자 ID
+//     * @return 지원 결과
+//     */
+//    @PostMapping("/apply")
+//    public ApiResponse<?> applyVolunteer(@RequestParam Long postId, @RequestParam Long userId) {
+//        volunteerService.applyVolunteer(postId, userId);
+//        return ApiResponse.createSuccessWithNoContent(ResponseCode.SUCCESS);
+//    }
+//
+//    /**
+//     * 모집글에 지원 취소하는 엔드포인트
+//     *
+//     * @param volunteerId 지원자 ID
+//     * @param userId 모집글 작성자 ID
+//     * @return 취소 결과
+//     */
+//    @DeleteMapping("/cancel")
+//    public ApiResponse<?> cancelVolunteer(@RequestParam Long volunteerId, @RequestParam Long userId) {
+//        volunteerService.cancelVolunteer(volunteerId, userId);
+//        return ApiResponse.createSuccessWithNoContent(ResponseCode.SUCCESS);
+//    }
+//
+//    /**
+//     * 모집글 지원자 목록 조회 엔드포인트 (참여자와 대기자)
+//     *
+//     * @param postId 모집글 ID
+//     * @param userId 작성자 ID
+//     * @return 지원자 목록
+//     */
+//    @GetMapping("/post/{postId}/volunteers")
+//    public ApiResponse<?> getPostVolunteerList(@PathVariable Long postId, @RequestParam Long userId) {
+//        VolunteerPostListResponse response = volunteerService.getPostVolunteerList(postId, userId);
+//        return ApiResponse.createSuccess(ResponseCode.SUCCESS, response);
+//    }
+//
+//
+//    /**
+//     * 모집글의 지원자를 수락하는 엔드포인트
+//     *
+//     * @param postId 모집글 ID
+//     * @param writerId 작성자 ID
+//     * @param targetId 지원자 ID
+//     * @return 수락 결과
+//     */
+//    @PostMapping("/{postId}/accept")
+//    public ApiResponse<?> acceptVolunteer(@PathVariable Long postId, @RequestParam Long writerId, @RequestParam Long targetId) {
+//        volunteerService.acceptVolunteer(postId, writerId, targetId);
+//        return ApiResponse.createSuccessWithNoContent(ResponseCode.SUCCESS);
+//    }
+//
+//    /**
+//     * 모집글의 지원자를 거절하는 엔드포인트
+//     *
+//     * @param postId 모집글 ID
+//     * @param writerId 작성자 ID
+//     * @param targetId 지원자 ID
+//     * @return 거절 결과
+//     */
+//    @PostMapping("/{postId}/refuse")
+//    public ApiResponse<?> refuseVolunteer(@PathVariable Long postId, @RequestParam Long writerId, @RequestParam Long targetId) {
+//        volunteerService.refusalVolunteer(postId, writerId, targetId);
+//        return ApiResponse.createSuccessWithNoContent(ResponseCode.SUCCESS);
+//    }
+//
+//}
--- findMuse-API/src/main/java/com/whh/findmuseapi/post/service/impl/VolunteerServiceImpl.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/post/service/impl/VolunteerServiceImpl.java
@@ -1,191 +1,191 @@
-package com.whh.findmuseapi.post.service.impl;
-
-import com.whh.findmuseapi.common.Exception.AlreadyExistException;
-import com.whh.findmuseapi.common.Exception.NotFoundException;
-import com.whh.findmuseapi.common.Exception.UnAuthorizationException;
-import com.whh.findmuseapi.common.constant.Infos;
-import com.whh.findmuseapi.post.dto.response.VolunteerMyPageListResponse;
-import com.whh.findmuseapi.post.dto.response.VolunteerPostListResponse;
-import com.whh.findmuseapi.post.dto.response.VolunteerReadResponse;
-import com.whh.findmuseapi.post.entity.Post;
-import com.whh.findmuseapi.post.entity.Volunteer;
-import com.whh.findmuseapi.post.repository.PostRepository;
-import com.whh.findmuseapi.post.repository.VolunteerRepository;
-import com.whh.findmuseapi.post.service.PostService;
-import com.whh.findmuseapi.post.service.VolunteerService;
-import com.whh.findmuseapi.user.entity.User;
-import com.whh.findmuseapi.user.repository.UserRepository;
-import java.util.List;
-import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-/**
- * class: VolunteerServiceImpl.
- *
- * @author devminseo
- * @version 8/24/24
- */
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class VolunteerServiceImpl implements VolunteerService {
-    private final PostService postService;
-    private final VolunteerRepository volunteerRepository;
-    private final UserRepository userRepository;
-    private final PostRepository postRepository;
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Long getInvitedCount(Post post) {
-        return volunteerRepository.countByPostIdAndStatusAndActiveStatus(post.getId(), Infos.InvieteStatus.ACCESS,
-                false);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean volunteerCheck(User user, Post post) {
-        return volunteerRepository.existsByUserAndPost(user, post);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public VolunteerPostListResponse getPostVolunteerList(Long postId, Long userId) {
-        User writer = userRepository.findById(userId)
-                .orElseThrow(() -> new NotFoundException("회원: " + userId));
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new NotFoundException("게시글: " + postId));
-
-        postService.checkWriter(writer, post);
-
-        List<Volunteer> waitingVolunteers = volunteerRepository.findByPostIdAndActiveStatusTrueAndStatus(postId,
-                Infos.InvieteStatus.Wait);
-
-        List<Volunteer> participationVolunteers = volunteerRepository.findByPostIdAndActiveStatusTrueAndStatusNot(postId,
-                Infos.InvieteStatus.Wait);
-
-        List<VolunteerReadResponse> waitingList = waitingVolunteers.stream()
-                .map(VolunteerReadResponse::toDto)
-                .collect(Collectors.toList());
-
-        List<VolunteerReadResponse> participationList = participationVolunteers.stream()
-                .map(VolunteerReadResponse::toDto)
-                .collect(Collectors.toList());
-
-        return VolunteerPostListResponse.toDto(participationList, waitingList);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public VolunteerMyPageListResponse getMyPageVolunteerList(Long userId) {
-        if (!userRepository.existsById(userId)) {
-            throw new NotFoundException("회원: " + userId);
-        }
-
-        List<Volunteer> approvalVolunteers = volunteerRepository.findByUserIdAndStatusAndActiveStatusTrue(userId,
-                Infos.InvieteStatus.Wait);
-        List<Volunteer> waitingVolunteers = volunteerRepository.findByUserIdAndStatusAndActiveStatusTrue(userId,
-                Infos.InvieteStatus.ACCESS);
-        List<Volunteer> refusalVolunteers = volunteerRepository.findByUserIdAndStatusAndActiveStatusTrue(userId,
-                Infos.InvieteStatus.DENY);
-
-        List<VolunteerReadResponse> approvalList = approvalVolunteers.stream()
-                .map(VolunteerReadResponse::toDto)
-                .collect(Collectors.toList());
-        List<VolunteerReadResponse> waitingList = waitingVolunteers.stream()
-                .map(VolunteerReadResponse::toDto)
-                .collect(Collectors.toList());
-
-        List<VolunteerReadResponse> refusalList = refusalVolunteers.stream()
-                .map(VolunteerReadResponse::toDto)
-                .collect(Collectors.toList());
-
-        return VolunteerMyPageListResponse.toDto(approvalList, waitingList, refusalList);
-    }
-
-
-    /**
-     * {@inheritDoc}
-     */
-    @Transactional
-    @Override
-    public void acceptVolunteer(Long postId, Long writer, Long targetId) {
-        User user = userRepository.findById(writer)
-                .orElseThrow(() -> new NotFoundException("회원: " + writer));
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new NotFoundException("게시글: " + postId));
-
-        postService.checkWriter(user, post);
-
-        Volunteer targetUser = volunteerRepository.findById(targetId).orElseThrow(()-> new NotFoundException(
-                "지원자: " + targetId));
-
-        targetUser.updateStatus(Infos.InvieteStatus.ACCESS);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Transactional
-    @Override
-    public void refusalVolunteer(Long postId, Long writer, Long targetId) {
-        User user = userRepository.findById(writer)
-                .orElseThrow(() -> new NotFoundException("회원: " + writer));
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new NotFoundException("게시글: " + postId));
-
-        postService.checkWriter(user, post);
-
-        Volunteer targetUser = volunteerRepository.findById(targetId).orElseThrow(()-> new NotFoundException(
-                "지원자: " + targetId));
-
-        targetUser.updateStatus(Infos.InvieteStatus.DENY);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Transactional
-    @Override
-    public void applyVolunteer(Long postId, Long userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NotFoundException("회원: " + userId));
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new NotFoundException("게시글: " + postId));
-
-        if (volunteerCheck(user, post)) {
-            throw new AlreadyExistException("지원자: " + userId);
-        }
-
-        Volunteer volunteer = Volunteer.toEntity(post, user);
-
-        volunteerRepository.save(volunteer);
-
-    }
-
-    /**
-     * {@inheritDoc}
-     * 지원자 유효성 검증.
-     */
-    @Transactional
-    @Override
-    public void cancelVolunteer(Long volunteerId, Long userId) {
-        Volunteer volunteer = volunteerRepository.findById(volunteerId).orElseThrow(()-> new NotFoundException(
-                "지원자: " + volunteerId));
-
-        if (!volunteer.getUser().getId().equals(userId)) {
-            throw new UnAuthorizationException("지원자");
-        }
-
-        volunteer.updateActiveStatus();
-    }
-}
+//package com.whh.findmuseapi.post.service.impl;
+//
+//import com.whh.findmuseapi.common.Exception.AlreadyExistException;
+//import com.whh.findmuseapi.common.exception.NotFoundException;
+//import com.whh.findmuseapi.common.exception.UnAuthorizationException;
+//import com.whh.findmuseapi.common.constant.Infos;
+//import com.whh.findmuseapi.post.dto.response.VolunteerMyPageListResponse;
+//import com.whh.findmuseapi.post.dto.response.VolunteerPostListResponse;
+//import com.whh.findmuseapi.post.dto.response.VolunteerReadResponse;
+//import com.whh.findmuseapi.post.entity.Post;
+//import com.whh.findmuseapi.post.entity.Volunteer;
+//import com.whh.findmuseapi.post.repository.PostRepository;
+//import com.whh.findmuseapi.post.repository.VolunteerRepository;
+//import com.whh.findmuseapi.post.service.PostService;
+//import com.whh.findmuseapi.post.service.VolunteerService;
+//import com.whh.findmuseapi.user.entity.User;
+//import com.whh.findmuseapi.user.repository.UserRepository;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+///**
+// * class: VolunteerServiceImpl.
+// *
+// * @author devminseo
+// * @version 8/24/24
+// */
+//@Service
+//@Transactional(readOnly = true)
+//@RequiredArgsConstructor
+//public class VolunteerServiceImpl implements VolunteerService {
+//    private final PostService postService;
+//    private final VolunteerRepository volunteerRepository;
+//    private final UserRepository userRepository;
+//    private final PostRepository postRepository;
+//
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Override
+//    public Long getInvitedCount(Post post) {
+//        return volunteerRepository.countByPostIdAndStatusAndActiveStatus(post.getId(), Infos.InvieteStatus.ACCESS,
+//                false);
+//    }
+//
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Override
+//    public boolean volunteerCheck(User user, Post post) {
+//        return volunteerRepository.existsByUserAndPost(user, post);
+//    }
+//
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Override
+//    public VolunteerPostListResponse getPostVolunteerList(Long postId, Long userId) {
+//        User writer = userRepository.findById(userId)
+//                .orElseThrow(() -> new NotFoundException("회원: " + userId));
+//        Post post = postRepository.findById(postId)
+//                .orElseThrow(() -> new NotFoundException("게시글: " + postId));
+//
+//        postService.checkWriter(writer, post);
+//
+//        List<Volunteer> waitingVolunteers = volunteerRepository.findByPostIdAndActiveStatusTrueAndStatus(postId,
+//                Infos.InvieteStatus.Wait);
+//
+//        List<Volunteer> participationVolunteers = volunteerRepository.findByPostIdAndActiveStatusTrueAndStatusNot(postId,
+//                Infos.InvieteStatus.Wait);
+//
+//        List<VolunteerReadResponse> waitingList = waitingVolunteers.stream()
+//                .map(VolunteerReadResponse::toDto)
+//                .collect(Collectors.toList());
+//
+//        List<VolunteerReadResponse> participationList = participationVolunteers.stream()
+//                .map(VolunteerReadResponse::toDto)
+//                .collect(Collectors.toList());
+//
+//        return VolunteerPostListResponse.toDto(participationList, waitingList);
+//    }
+//
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Override
+//    public VolunteerMyPageListResponse getMyPageVolunteerList(Long userId) {
+//        if (!userRepository.existsById(userId)) {
+//            throw new NotFoundException("회원: " + userId);
+//        }
+//
+//        List<Volunteer> approvalVolunteers = volunteerRepository.findByUserIdAndStatusAndActiveStatusTrue(userId,
+//                Infos.InvieteStatus.Wait);
+//        List<Volunteer> waitingVolunteers = volunteerRepository.findByUserIdAndStatusAndActiveStatusTrue(userId,
+//                Infos.InvieteStatus.ACCESS);
+//        List<Volunteer> refusalVolunteers = volunteerRepository.findByUserIdAndStatusAndActiveStatusTrue(userId,
+//                Infos.InvieteStatus.DENY);
+//
+//        List<VolunteerReadResponse> approvalList = approvalVolunteers.stream()
+//                .map(VolunteerReadResponse::toDto)
+//                .collect(Collectors.toList());
+//        List<VolunteerReadResponse> waitingList = waitingVolunteers.stream()
+//                .map(VolunteerReadResponse::toDto)
+//                .collect(Collectors.toList());
+//
+//        List<VolunteerReadResponse> refusalList = refusalVolunteers.stream()
+//                .map(VolunteerReadResponse::toDto)
+//                .collect(Collectors.toList());
+//
+//        return VolunteerMyPageListResponse.toDto(approvalList, waitingList, refusalList);
+//    }
+//
+//
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Transactional
+//    @Override
+//    public void acceptVolunteer(Long postId, Long writer, Long targetId) {
+//        User user = userRepository.findById(writer)
+//                .orElseThrow(() -> new NotFoundException("회원: " + writer));
+//        Post post = postRepository.findById(postId)
+//                .orElseThrow(() -> new NotFoundException("게시글: " + postId));
+//
+//        postService.checkWriter(user, post);
+//
+//        Volunteer targetUser = volunteerRepository.findById(targetId).orElseThrow(()-> new NotFoundException(
+//                "지원자: " + targetId));
+//
+//        targetUser.updateStatus(Infos.InvieteStatus.ACCESS);
+//    }
+//
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Transactional
+//    @Override
+//    public void refusalVolunteer(Long postId, Long writer, Long targetId) {
+//        User user = userRepository.findById(writer)
+//                .orElseThrow(() -> new NotFoundException("회원: " + writer));
+//        Post post = postRepository.findById(postId)
+//                .orElseThrow(() -> new NotFoundException("게시글: " + postId));
+//
+//        postService.checkWriter(user, post);
+//
+//        Volunteer targetUser = volunteerRepository.findById(targetId).orElseThrow(()-> new NotFoundException(
+//                "지원자: " + targetId));
+//
+//        targetUser.updateStatus(Infos.InvieteStatus.DENY);
+//    }
+//
+//    /**
+//     * {@inheritDoc}
+//     */
+//    @Transactional
+//    @Override
+//    public void applyVolunteer(Long postId, Long userId) {
+//        User user = userRepository.findById(userId)
+//                .orElseThrow(() -> new NotFoundException("회원: " + userId));
+//        Post post = postRepository.findById(postId)
+//                .orElseThrow(() -> new NotFoundException("게시글: " + postId));
+//
+//        if (volunteerCheck(user, post)) {
+//            throw new AlreadyExistException("지원자: " + userId);
+//        }
+//
+//        Volunteer volunteer = Volunteer.toEntity(post, user);
+//
+//        volunteerRepository.save(volunteer);
+//
+//    }
+//
+//    /**
+//     * {@inheritDoc}
+//     * 지원자 유효성 검증.
+//     */
+//    @Transactional
+//    @Override
+//    public void cancelVolunteer(Long volunteerId, Long userId) {
+//        Volunteer volunteer = volunteerRepository.findById(volunteerId).orElseThrow(()-> new NotFoundException(
+//                "지원자: " + volunteerId));
+//
+//        if (!volunteer.getUser().getId().equals(userId)) {
+//            throw new UnAuthorizationException("지원자");
+//        }
+//
+//        volunteer.updateActiveStatus();
+//    }
+//}
