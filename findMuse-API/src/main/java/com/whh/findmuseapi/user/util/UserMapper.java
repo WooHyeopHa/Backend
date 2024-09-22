c488cd58abd751041e0d093cd430c5cd712d0382
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2024-09-22 18:44:29 +0900

    [fix] birthYear 검증 단계 추가

--- findMuse-API/src/main/java/com/whh/findmuseapi/user/util/UserMapper.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/user/util/UserMapper.java
@@ -1,5 +1,6 @@
 package com.whh.findmuseapi.user.util;
 
+import com.whh.findmuseapi.common.Exception.CustomBadRequestException;
 import com.whh.findmuseapi.common.constant.Infos;
 import com.whh.findmuseapi.user.dto.request.UserProfileInformationRequest;
 import com.whh.findmuseapi.user.dto.request.UserProfileLocationRequest;
@@ -10,11 +11,14 @@ import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+import java.time.LocalDate;
+
+@Mapper()
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     @Mapping(target = "gender", source = "gender", qualifiedByName = "stringToGender")
+    @Mapping(target = "birthYear", source = "birthYear", qualifiedByName = "stringToInteger")
     void updateUserFromProfileInformation(UserProfileInformationRequest userProfileInformationRequest, @MappingTarget User user);
 
     void updateUserFromProfileLocation(UserProfileLocationRequest userProfileLocationRequest, @MappingTarget User user);
@@ -22,5 +26,20 @@ public interface UserMapper {
     @Named("stringToGender")
     default Infos.Gender stringToGender(String gender) {
         return Infos.Gender.convertStringToGender(gender);
+    }
+
+    @Named("stringToInteger")
+    default Integer stringToInteger(String integer) {
+        if (integer == null || integer.isEmpty()) {
+            throw new CustomBadRequestException("생년월일 값을 전달하지 않았습니다.");
+        }
+        if (!integer.matches("\\d+")) {
+            throw new CustomBadRequestException("생년월일에 문자열이 포함되어 있습니다.");
+        }
+        Integer integerValue = Integer.valueOf(integer);
+        if (integerValue > LocalDate.now().getYear() || integerValue < 1900) {
+            throw new CustomBadRequestException("생년월일을 정확히 입력하세요.");
+        }
+        return integerValue;
     }
 }
