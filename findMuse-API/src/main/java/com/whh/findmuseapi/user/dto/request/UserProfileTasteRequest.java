00540024b37893e33d7fe7cefe85d62b04ae0d94
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2024-09-24 08:13:31 +0900

    [fix] convert dto class to record

--- findMuse-API/src/main/java/com/whh/findmuseapi/user/dto/request/UserProfileTasteRequest.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/user/dto/request/UserProfileTasteRequest.java
@@ -1,18 +1,9 @@
 package com.whh.findmuseapi.user.dto.request;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.List;
 
-@Getter
-public class UserProfileTasteRequest {
-    private List<TasteSelection> tastes;
+public record UserProfileTasteRequest(List<TasteSelection> tastes) {
 
-    @Getter
-    @Setter
-    public static class TasteSelection {
-        private String category;
-        private List<String> selections;
+    public record TasteSelection(String category, List<String> selections) {
     }
 }
