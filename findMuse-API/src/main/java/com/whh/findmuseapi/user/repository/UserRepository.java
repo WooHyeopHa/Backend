eefa3bd2a4cec1f39a7d63ac781c1feed9b053bb
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2024-09-29 19:55:29 +0900

    [fix] LazyFetchInitialize를 해결하기 위한 자체 fetch join

--- findMuse-API/src/main/java/com/whh/findmuseapi/user/repository/UserRepository.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/user/repository/UserRepository.java
@@ -3,15 +3,18 @@ package com.whh.findmuseapi.user.repository;
 import com.whh.findmuseapi.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> findByAccountId(String accountId);
-    Optional<User> findByEmail(Optional<String> email);
     Optional<User> findByEmail(String email);
     Optional<User> findByRefreshToken(String refreshToken);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.userTastes WHERE u.id = :userId")
+    Optional<User> findUserWithTastesById(@Param("userId") Long userId);
 
     boolean existsByNickname(String nickname);
 }
