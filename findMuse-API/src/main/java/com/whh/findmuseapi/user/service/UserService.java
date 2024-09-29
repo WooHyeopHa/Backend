73091617d57ce8b6939a84bd9dd808d4d9a89bc9
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2024-09-29 23:43:33 +0900

    [feat] 마이페이지 메인 화면

--- findMuse-API/src/main/java/com/whh/findmuseapi/user/service/UserService.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/user/service/UserService.java
@@ -4,8 +4,6 @@ import com.whh.findmuseapi.common.constant.Infos;
 import com.whh.findmuseapi.common.exception.CustomBadRequestException;
 import com.whh.findmuseapi.common.util.S3Uploader;
 import com.whh.findmuseapi.user.dto.request.UserProfile;
-import com.whh.findmuseapi.user.dto.request.UserProfileInformationRequest;
-import com.whh.findmuseapi.user.dto.request.UserProfileTasteRequest;
 import com.whh.findmuseapi.user.dto.response.NicknameDuplicationResponse;
 import com.whh.findmuseapi.user.entity.Taste;
 import com.whh.findmuseapi.user.entity.User;
@@ -21,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -36,7 +35,9 @@ public class UserService {
     @Description("사용자 닉네임 설정")
     @Transactional
     public void registerProfileNickname(User user, UserProfile.NicknameRequest nicknameRequest) {
+        if (nicknameRequest.nickname().isEmpty() || nicknameRequest.nickname() == null) throw new CustomBadRequestException("닉네임을 입력해주세요.");
         if (userRepository.existsByNickname(nicknameRequest.nickname())) throw new CustomBadRequestException("존재하는 닉네임입니다.");
+
         user.updateNickname(nicknameRequest.nickname());
         user.authorizeUser();
     }
@@ -52,13 +53,24 @@ public class UserService {
 
     @Description("사용자 정보 설정")
     @Transactional
-    public void registerProfileInformation(User user, UserProfileInformationRequest informationRequest, MultipartFile profileImage) throws IOException {
+    public void registerProfileInformation(User user, UserProfile.InformationRequest informationRequest, MultipartFile profileImage) throws IOException {
         user.updateInformation(
                 validateBirthYear(informationRequest.birthYear()),
                 Infos.Gender.convertStringToGender(informationRequest.gender())
         );
 
-        user.updateProfileImageUrl(s3Uploader.upload(profileImage, Infos.ResourceUrl.PROFILE_IMAGE.getUrl()  + user.getEmail()));
+
+        if (profileImage != null) {
+            Optional.ofNullable(user.getProfileImageUrl())
+                    .ifPresent(s3Uploader::deleteFile);
+
+            String newImageUrl = s3Uploader.upload(
+                    profileImage,
+                    Infos.ResourceUrl.PROFILE_IMAGE.getUrl() + user.getEmail()
+            );
+            user.updateProfileImageUrl(newImageUrl);
+        }
+
         userRepository.save(user);
         log.info("사용자 정보 설정 완료 : {}", user.getId());
     }
@@ -72,7 +84,7 @@ public class UserService {
 
     @Description("사용자 취향 설정")
     @Transactional
-    public void registerProfileTaste(Long userId, UserProfileTasteRequest userProfileTasteRequest) {
+    public void registerProfileTaste(Long userId, UserProfile.TasteRequest userProfileTasteRequest) {
         User user = userRepository.findUserWithTastesById(userId)
                 .orElseThrow(() -> new CustomBadRequestException("사용자를 찾을 수 없습니다."));
 
@@ -100,12 +112,25 @@ public class UserService {
 
     @Description("사용자 취향 수정")
     @Transactional
-    public void updateProfileTaste(Long userId, UserProfileTasteRequest userProfileTasteRequest) {
+    public void updateProfileTaste(Long userId, UserProfile.TasteRequest userProfileTasteRequest) {
         User user = userRepository.findUserWithTastesById(userId)
                         .orElseThrow(() -> new CustomBadRequestException("사용자를 찾을 수 없습니다."));
         user.getUserTastes().clear();
 
         registerProfileTaste(userId, userProfileTasteRequest);
+    }
+
+    @Description("프로필 편집")
+    @Transactional
+    public void updateProfile(User user, UserProfile.ChangeRequest changeRequest, MultipartFile profileImage) throws IOException {
+        registerProfileNickname(user, changeRequest.nicknameRequest());
+        registerProfileLocation(user, changeRequest.locationRequest());
+        registerProfileInformation(user, changeRequest.informationRequest(), profileImage);
+        user.updateProfile(changeRequest.comment(), changeRequest.showStatus());
+        userRepository.save(user);
+
+        log.info("사용자 닉네임 : {}, 위치 : {}, 생년 : {}, 성별 : {}, 한줄소개 : {}, 활동내역 공개 여부 : {}, 프로필 이미지 Url : {}",
+                user.getNickname(), user.getLocation(), user.getBirthYear(), user.getGender().getInfo(), user.getComment(), user.isShowStatus(), user.getProfileImageUrl());
     }
 
     private Integer validateBirthYear(String integer) {
--- findMuse-API/src/main/java/com/whh/findmuseapi/user/util/UserMapper.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/user/util/UserMapper.java
@@ -1,9 +1,62 @@
 package com.whh.findmuseapi.user.util;
 
-import org.mapstruct.*;
-import org.mapstruct.factory.Mappers;
+import com.whh.findmuseapi.art.entity.ArtHistory;
+import com.whh.findmuseapi.art.entity.ArtLike;
+import com.whh.findmuseapi.common.constant.MuseLevel;
+import com.whh.findmuseapi.user.dto.response.MyInfo;
+import com.whh.findmuseapi.user.entity.User;
+import lombok.RequiredArgsConstructor;
 
-@Mapper()
-public interface UserMapper {
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserMapper {
+
+    public static MyInfo toMyInfo(User user, Long artCount, List<ArtLike> artLikes, List<ArtHistory> histories) {
+        return MyInfo.builder()
+                .userSimpleResponse(toUserSimpleResponse(user))
+                .userActivityResponse(toUserActivityResponse(user, artLikes, histories))
+                .userLevelResponse(toMyLevelResponse(user))
+                .eventResponse(toEventResponse(user, artCount))
+                .build();
+    }
+
+    public static MyInfo.UserSimpleResponse toUserSimpleResponse(User user) {
+        return MyInfo.UserSimpleResponse.builder()
+                .nickname(user.getNickname())
+                .location(user.getLocation())
+                .birthYear(user.getBirthYear())
+                .comment(user.getComment())
+                .build();
+    }
+
+    public static MyInfo.UserActivityResponse toUserActivityResponse(User user, List<ArtLike> artLikes, List<ArtHistory> histories) {
+        return MyInfo.UserActivityResponse.builder()
+                .eventReview(user.getUserReviews().size())
+                .hasViewed(histories.size()) // histories의 크기를 사용
+                .wantToSee(artLikes.size())   // artLikes의 크기를 사용
+                .build();
+    }
+
+    public static MyInfo.UserLevelResponse toMyLevelResponse(User user) {
+        MuseLevel museLevel = MuseLevel.determineLevel(user.getArtCount(), user.getFindMuseCount());
+        int denominator = museLevel.getFindMuseCount() + museLevel.getViewCount();
+        int numerator = Math.min(user.getArtCount(), museLevel.getViewCount())
+                + Math.min(user.getFindMuseCount(), museLevel.getFindMuseCount());
+
+        double percentage = ((double) numerator / denominator) * 100;
+
+        return MyInfo.UserLevelResponse.builder()
+                .level(museLevel.getLevel())
+                .description(museLevel.getDescription())
+                .percentage(percentage)
+                .build();
+    }
+
+    public static MyInfo.EventResponse toEventResponse(User user, Long artCount) {
+        return MyInfo.EventResponse.builder()
+                .artCount(artCount)
+                .participatedCount(user.getArtCount() + user.getUserReviews().size())
+                .build();
+    }
 }
