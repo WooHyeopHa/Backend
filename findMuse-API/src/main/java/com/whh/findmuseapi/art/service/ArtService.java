f462f9d8fef1d0eaaad9cbf0b7e5056cadf3b527
kyu-hyun <dlrbgus722@naver.com>
2024-09-30 23:13:29 +0900

    feat : 문화예술 서비스

--- findMuse-API/src/main/java/com/whh/findmuseapi/art/service/ArtService.java
+++ findMuse-API/src/main/java/com/whh/findmuseapi/art/service/ArtService.java
@@ -1,7 +1,22 @@
 package com.whh.findmuseapi.art.service;
 
-import org.springframework.stereotype.Service;
+import com.whh.findmuseapi.art.dto.ArtHomeResponse;
+import com.whh.findmuseapi.art.dto.ArtLikeRequest;
+import com.whh.findmuseapi.art.dto.ArtListResponse;
+import com.whh.findmuseapi.art.dto.ArtOneResponse;
 
-@Service
-public class ArtService {
+import java.util.List;
+
+
+public interface ArtService {
+
+    ArtOneResponse getArtInfoOne(Long artId);
+
+    ArtListResponse getArtByCondition(Long userId, String date, List<String> genre, String sort);
+
+    void markLike(ArtLikeRequest artLikeRequest);
+
+    ArtListResponse getArtByRank(Long userId, String genre);
+
+    ArtHomeResponse getArtByHome(Long userId);
 }
