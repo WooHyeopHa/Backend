9e48ae26fec67a631a4414a285dff720dfe00c51
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2024-10-06 00:44:54 +0900

    [feat] repository 설정

---
+++ findMuse-chatting/src/main/java/com/whh/findmusechatting/chat/repository/ChatMessageRepository.java
@@ -0,0 +1,13 @@
+package com.whh.findmusechatting.chat.repository;
+
+import com.whh.findmusechatting.chat.entity.ChatMessage;
+import com.whh.findmusechatting.common.constant.MessageStatus;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ChatMessageRepository extends ReactiveMongoRepository<ChatMessage, String> {
+
+    Mono<Long> countBySenderIdAndReceiverIdAndStatus(String senderId, String receiverId, MessageStatus status);
+    Flux<ChatMessage> findByChatId(String chatId);
+}
