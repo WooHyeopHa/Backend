349f778150629c6f2b6fe379bb0e5118e1a4052b
ehyeok9 <e_hyeok9@kookmin.ac.kr>
2024-10-30 18:22:00 +0900

    [feat] mongodb 설정

--- findMuse-chatting/src/main/java/com/whh/findmusechatting/FindMuseChattingApplication.java
+++ findMuse-chatting/src/main/java/com/whh/findmusechatting/FindMuseChattingApplication.java
@@ -2,12 +2,12 @@ package com.whh.findmusechatting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+@EnableReactiveMongoRepositories
 @SpringBootApplication
 public class FindMuseChattingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FindMuseChattingApplication.class, args);
 	}
-
 }
